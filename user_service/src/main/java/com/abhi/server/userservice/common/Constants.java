3ca8fe6649c631d6259d87e5922ef1038c8ce3f2
ajulka <abhishek.julka@oracle.com>
2018-08-24 12:21:31 -0700

    refactoring

--- user_service/src/main/java/com/abhi/server/userservice/common/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.abhi.server.userservice.common;
-
-public class Constants {
-    public static String ENDPOINT_HELLO = "hello";
-    public static String ENDPOINT_USER = "user";
-    public static String ENDPOINT_BEGIN_ENROLLMENT = "BeginEnrollment";
-}
--- user_service/src/main/java/com/abhi/server/userservice/manager/impl/BaseResourceManager.java
+++
@@ -1,9 +0,0 @@
-package com.abhi.server.userservice.manager.impl;
-
-import com.abhi.server.userservice.entity.BaseEntity;
-
-public abstract class BaseResourceManager {
-    protected abstract void updateMetaData(BaseEntity resource, String metaData);
-    protected abstract void updateCreatedTime(BaseEntity resource, String date);
-
-}
