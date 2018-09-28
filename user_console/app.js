8720cdb6d23c10cd901ec9fed3026314af6721c6
ajulka <julka.abhi@gmail.com>
2018-09-27 19:13:10 -0700

    jade

--- user_console/app.js
+++ user_console/app.js
@@ -21,7 +21,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("Welcome1"));
-app.use(session());
+app.use(session({
+	secret: "Welcome1",
+	saveUninitialized: true,
+	resave:true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(checkAuth);
 app.use(flash());
